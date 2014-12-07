64eb3e79e67c6dba27fd6cc50cdaaabcad65db1c
ludmilas <alexss_09@mail.ru>
2014-12-07 15:08:28 +0400

    Initial commit

---
+++ view/forma_insert.php
@@ -0,0 +1,6 @@
+
+<form action="controller.php" method="post">
+<input type="text" name="title">Введите заголовок новости<br></input>
+    <input type="text" name="new">Введите новость<br></input>
+    <button type="submit">Сохранить </button>
+</form>
