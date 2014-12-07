8d2920f571e2aa905b831d9ba1b048def8d0bdba
ludmilas <alexss_09@mail.ru>
2014-12-07 14:17:00 +0400

    Initial commit

---
+++ view/index1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Новости</title>
+</head>
+<body>
+<form action="HTTP://site/test/index.php" method="post">
+<input type="text" name="title">Введите заголовок новости<br></input>
+    <input type="text" name="new">Введите новость<br></input>
+    <button type="submit">Сохранить </button>
+</form>
+</body>
+</html>
