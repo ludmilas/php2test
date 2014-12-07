b459bea0fa4fe11221bcacc11541fd36ff31d1b3
ludmilas <alexss_09@mail.ru>
2014-12-07 15:06:58 +0400

    Initial commit

---
+++ controller.php
@@ -0,0 +1,11 @@
+<?php
+
+    require_once __DIR__ . '/models/news.php';
+
+if (isset($_POST['title']) and isset($_POST['new']))
+{
+    $name_new=$_POST['title'];
+    $text_new=$_POST['new'];
+    News_insert($name_new,$text_new);
+}
+header('location: index.php');
