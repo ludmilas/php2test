787fe73a5a1c1ef7ef8ffbb03265fc259b532cc6
ludmilas <alexss_09@mail.ru>
2014-12-12 07:18:34 +0300

    Initial commit

--- view/forma_redact.php
+++ view/forma_redact.php
@@ -1,13 +1,7 @@
-<html>
-<head>
-    <title>Новости</title>
-</head>
-<body>
-<?php foreach ($r as $article): ?>
-    <forma>
-        <input type="text" name="title" value="<?=$article['title'];?>"> Поле для редактирования заголовка </input><br>
-        <input type="text" name="text" value="<?=$article['text'];?>"> Поле для редактирования новости </input><br>
+
+
+    <forma action="red_new_dob.php" method="post">
+        <input type="text" name="title" value="<?=$r['title'];?>"> Поле для редактирования заголовка </input><br>
+        <input type="text" name="text" value="<?=$r['text'];?>"> Поле для редактирования новости </input><br>
+        <input type="submit" value="Добавить">
     </forma>
-<?php endforeach; ?>
-</body>
-</html>
