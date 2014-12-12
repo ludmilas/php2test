6c23ab15b8eb43c88538a8d9301d52ade2056f0b
ludmilas <alexss_09@mail.ru>
2014-12-12 23:45:04 +0300

    Initial commit

--- view/forma_redact.php
+++ view/forma_redact.php
@@ -1,7 +1,9 @@
 
+<?php
 
-    <forma action="red_new_dob.php" method="post">
-        <input type="text" name="title" value="<?=$r['title'];?>"> Поле для редактирования заголовка </input><br>
-        <input type="text" name="text" value="<?=$r['text'];?>"> Поле для редактирования новости </input><br>
+?>
+    <form action="red_new_dob.php?id=<?php echo $id ?>" method="post">
+        <input type="text" name="title" value="<?=$r[0]['title'];?>"> Поле для редактирования заголовка </input><br>
+        <input type="text" name="text" value="<?=$r[0]['text'];?>"> Поле для редактирования новости </input><br>
         <input type="submit" value="Добавить">
-    </forma>
+    </form>
