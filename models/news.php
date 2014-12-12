57fade154a8236745f591e47942eb80fd8762bb8
ludmilas <alexss_09@mail.ru>
2014-12-12 23:44:40 +0300

    Initial commit

--- models/news.php
+++ models/news.php
@@ -1,9 +1,10 @@
 <?php
 
 require_once __DIR__ . '/../class/cl.php';
+require_once __DIR__ . '/../class/clmod.php';
 
-
-function News_getAll()
+$st = new Newmod();
+/*function News_getAll()
 {
     $base = new Database();
     return $base->DBQuery("
@@ -24,14 +25,14 @@ function Oun_new($i)
     return $base->DBQuery("
     SELECT * FROM news WHERE id=$i
     ");
-function New_up($i,$title,$new)
+}
+    function New_up($i,$title,$new)
 {
     $base = new Database();
-    return $base->DBQuery("UPDATE
-SET `title`=$title, `text`=$new,…
-WHERE id=$i");
+    return $base->DBExec(" UPDATE news
+SET title='$title',text='$new' WHERE id=$i");
 
 }
+*/
 
 
-}
