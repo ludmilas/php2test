e9e22b67fca0fdeb9c28dd1078606c35ae8e574c
ludmilas <alexss_09@mail.ru>
2014-12-12 23:47:08 +0300

    Initial commit

--- red_new_dob.php
+++ red_new_dob.php
@@ -1,10 +1,10 @@
 <?php
 require_once __DIR__ . '/models/news.php';
-
+$id=$_GET['id'];
 if (isset($_POST['title']) and isset($_POST['text'])) {
     $name_new = $_POST['title'];
     $text_new = $_POST['text'];
-    $s = New_up($id, $name_new, $text_new);
+    $s = $st->New_up($id, $name_new, $text_new);
 
 }
 if ($s === false) {
