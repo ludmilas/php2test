39d195c0871154969584348120ef36ab9919a7ae
ludmilas <alexss_09@mail.ru>
2014-12-12 07:17:01 +0300

    Initial commit

--- models/news.php
+++ models/news.php
@@ -2,10 +2,10 @@
 
 require_once __DIR__ . '/../class/cl.php';
 
-$base = new Database();
-var_dump($base);
+
 function News_getAll()
 {
+    $base = new Database();
     return $base->DBQuery("
     SELECT * FROM news
     ");
@@ -13,14 +13,25 @@ function News_getAll()
 
 function News_insert($title,$new)
 {
-
+    $base = new Database();
     $base->DBExec("INSERT INTO `news` (`title`, `text`) VALUES ('$title', '$new')");
 
 }
 function Oun_new($i)
 
 {
+    $base = new Database();
     return $base->DBQuery("
     SELECT * FROM news WHERE id=$i
     ");
+function New_up($i,$title,$new)
+{
+    $base = new Database();
+    return $base->DBQuery("UPDATE
+SET `title`=$title, `text`=$new,…
+WHERE id=$i");
+
+}
+
+
 }
