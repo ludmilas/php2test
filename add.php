d3adf527ed441db17de15c7241e6eff629f7930d
ludmilas <alexss_09@mail.ru>
2014-12-12 07:18:56 +0300

    Initial commit

--- add.php
+++ add.php
@@ -2,13 +2,18 @@
 
     require_once __DIR__ . '/models/news.php';
 
-if (isset($_POST['title']) and isset($_POST['new']))
+if (isset($_POST['title']) and isset($_POST['new'])) {
+    $name_new = $_POST['title'];
+    $text_new = $_POST['new'];
+   $r = News_insert($name_new, $text_new);
+
+     }
+    if ($r === false) {
+        echo 'ошибка';
+    }
+else
 {
-    $name_new=$_POST['title'];
-    $text_new=$_POST['new'];
-    News_insert($name_new,$text_new);
+
+    header('location:index.php');
 }
 
-
-
-header('location: index.php');
