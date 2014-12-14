1e75f556d1e1908a7d38eaab8f0b05d89054d308
ludmilas <alexss_09@mail.ru>
2014-12-15 00:23:50 +0300

    Initial commit

--- view/index.php
+++ view/index.php
@@ -1,9 +1,11 @@
+
 <html>
 <head>
     <title>Новости</title>
 </head>
 <body>
-    <?php foreach ($news as $article): ?>
+
+     <?php foreach ($Views as $article): ?>
     <article>
         <h1><?=$article['title'];?></h1>
         <div><?=$article['text'];?></div>
