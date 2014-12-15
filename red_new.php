8a328d62060be76eabfb09d3ca1a7293db486423
ludmilas <alexss_09@mail.ru>
2014-12-15 22:43:01 +0300

    Initial commit

--- red_new.php
+++ red_new.php
@@ -1,7 +1,11 @@
 <?php
 
 require_once __DIR__ . '/models/news.php';
+require_once __DIR__ . '/class/View.php';
 $id=$_GET['id'];
-$r=$st->Oun_new($id);
-include ('view/forma_redact.php');
-
+$Views = new View(view);
+$Views->redact = $st->Oun_new($id);
+$html = $Views->display('forma_redact.php');
+echo $html;
+//$redact = $st->Oun_new($id);
+//include('view/forma_redact.php');
