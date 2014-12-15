b6e082ead14cadc46bd7d727dfcafc52d6d82239
ludmilas <alexss_09@mail.ru>
2014-12-15 22:42:36 +0300

    Initial commit

--- new.php
+++ new.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once __DIR__ . '/models/news.php';
+require_once __DIR__ . '/class/View.php';
 $id=$_GET['id'];
-$r=$st->Oun_new($id);
-include ('view/oun_new.php');
+$Views = new View(view);
+$Views->oun = $st->Oun_new($id);
+$html = $Views->display('oun_new.php');
+echo $html;
