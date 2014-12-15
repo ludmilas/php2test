3c93c0c66c5c77bc0a83fc111638e4d9b1e69f57
ludmilas <alexss_09@mail.ru>
2014-12-15 22:40:36 +0300

    Initial commit

--- class/View.php
+++ class/View.php
@@ -8,11 +8,15 @@ class View extends Storage{
     }
    public function display($template)
    {
-      // ob_start();
-       include $this->p.'/'.$template;
-       //$ret = ob_get_contents();
-      // ob_get_clean();
-      // return $ret;
+
+       ob_start();
+           foreach ($this as $key => $value) {
+               $$key = $value;
+           }
+               include $this->p.'/'.$template;
+       $ret = ob_get_contents();
+      ob_get_clean();
+       return $ret;
 
    }
 }
