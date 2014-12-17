a1cc61a9ce03cb8a5ddefbc1f002cb03489abe94
ludmilas <alexss_09@mail.ru>
2014-12-18 00:23:13 +0300

    ДЗ4

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
+require 'boot.php';
+$controller = new NewsController();
+$controller->action('all');
+/*$route = $_GET['r'];
+$routeParts= explode('/', $route);
+$controllerClassName = ucfirst($routeParts[0]).'Controller';
 
-require_once __DIR__ . '/models/news.php';
-require_once __DIR__ . '/class/View.php';
-$Views = new View(view);
 
-$Views->news = $st->News_getAll();
-
-
-$html = $Views->display('index.php');
-
-echo $html;
+$controller = new $controllerClassName;
+$controller->action($routeParts[1]);*/
