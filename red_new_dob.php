417e8848311517b404077d394a37eec745799862
ludmilas <alexss_09@mail.ru>
2014-12-18 00:24:28 +0300

    ДЗ4

--- red_new_dob.php
+++ red_new_dob.php
@@ -1,9 +1,10 @@
 <?php
-require_once __DIR__ . '/models/news.php';
+require 'boot.php';
 $id=$_GET['id'];
 if (isset($_POST['title']) and isset($_POST['text'])) {
     $name_new = $_POST['title'];
     $text_new = $_POST['text'];
+    $st = new Newmod();
     $s = $st->New_up($id, $name_new, $text_new);
 
 }
