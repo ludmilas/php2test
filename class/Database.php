42553de5a803ba241700da3ab3a4c99a1d023f60
ludmilas <alexss_09@mail.ru>
2014-12-18 00:18:34 +0300

    ДЗ4

---
+++ class/Database.php
@@ -0,0 +1,47 @@
+<?php
+class Database
+{
+
+    private function config()
+    {
+        return include __DIR__ . '/../config.php';
+    }
+
+    public function __construct()
+    {
+        $config = $this->config();
+        mysql_connect(
+            $config['db']['host'],
+            $config['db']['user'],
+            $config['db']['password']
+        );
+        mysql_select_db($config['db']['dbname']);
+    }
+
+    public function DBQuery($sql)
+    {
+        $res = mysql_query($sql);
+        if (!$res) {
+            echo mysql_error();
+            return array();
+        }
+
+        $ret = array();
+        while ($row = mysql_fetch_assoc($res)) {
+            $ret[] = $row;
+        }
+        return $ret;
+    }
+
+    public function DBExec($sql)
+    {
+        $res = mysql_query($sql);
+        if ($res === false) {
+
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
