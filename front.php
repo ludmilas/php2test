460e543ff3e8efd03d8e359cdf0bb217b606b29e
ludmilas <alexss_09@mail.ru>
2014-12-18 00:23:01 +0300

    ДЗ4

---
+++ front.php
@@ -0,0 +1,10 @@
+<?php
+require 'boot.php';
+
+$route = $_GET['r'];
+$routeParts= explode('/', $route);
+$controllerClassName = ucfirst($routeParts[0]).'Controller';
+
+
+$controller = new $controllerClassName;
+$controller->action($routeParts[1]);
