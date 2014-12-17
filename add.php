9b85f5b93043632d0b096ec1b64ff4920489c436
ludmilas <alexss_09@mail.ru>
2014-12-18 00:20:23 +0300

    ДЗ4

--- add.php
+++ add.php
@@ -1,10 +1,11 @@
 <?php
 
-    require_once __DIR__ . '/models/news.php';
+require 'boot.php';
 
 if (isset($_POST['title']) and isset($_POST['new'])) {
     $name_new = $_POST['title'];
     $text_new = $_POST['new'];
+    $st = new Newmod();
    $r =$st->News_insert($name_new, $text_new);
 
      }
