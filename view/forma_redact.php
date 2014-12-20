98b23249f09b46d3d5a92124b43931ebaf8f1aca
ludmilas <alexss_09@mail.ru>
2014-12-20 23:37:55 +0300

    ДЗ4

--- view/forma_redact.php
+++ view/forma_redact.php
@@ -2,7 +2,7 @@
 <?php
 
 ?>
-    <form action="red_new_dob.php?id=<?php echo $redact[0]['id']; ?>" method="post">
+    <form action="index.php?id=<?php echo $redact[0]['id']; ?>&r=news/rednew" method="post">
         <input type="text" name="title" value="<?=$redact[0]['title'];?>"> Поле для редактирования заголовка </input><br>
         <input type="text" name="text" value="<?=$redact[0]['text'];?>"> Поле для редактирования новости </input><br>
         <input type="submit" value="Добавить">
