d74e9269a97bbdf762681aaaf98f70e342ac944d
ludmilas <alexss_09@mail.ru>
2014-12-20 23:37:39 +0300

    ДЗ4

--- view/forma_new.php
+++ view/forma_new.php
@@ -1,5 +1,5 @@
 <?php foreach ($news as $article): ?>
     <article>
-        <a href="new.php?id=<?=$article['id'];?>"><?=$article['title'];?></a>
+        <a href="index.php?id=<?=$article['id'];?>&r=news/ounnew"><?=$article['title'];?></a>
 </article>
 <?php endforeach; ?>
