b796b0e3e721af4e1c7009df4e75410bdc8fcbd7
ludmilas <alexss_09@mail.ru>
2014-12-20 23:37:21 +0300

    ДЗ4

--- view/forma_insert.php
+++ view/forma_insert.php
@@ -1,5 +1,5 @@
 
-<form action="add.php" method="post">
+<form action="index.php?r=news/add" method="post">
 <input type="text" name="title">Введите заголовок новости<br></input>
     <input type="text" name="new">Введите новость<br></input>
     <input type="submit" value="Добавить">
