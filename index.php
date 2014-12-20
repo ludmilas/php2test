8c5089f1b6fff85177475838c3aefa53b8c9dad8
ludmilas <alexss_09@mail.ru>
2014-12-20 23:39:47 +0300

    ДЗ4

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 require 'boot.php';
-$controller = new NewsController();
-$controller->action('all');
-/*$route = $_GET['r'];
+/*$controller = new NewsController();
+$controller->action('all');*/
+$route = $_GET['r'];
 $routeParts= explode('/', $route);
 $controllerClassName = ucfirst($routeParts[0]).'Controller';
 
 
 $controller = new $controllerClassName;
-$controller->action($routeParts[1]);*/
+$controller->action($routeParts[1]);
