9138d72d19e7e10d5a826dcb2346183a30ecf882
ludmilas <alexss_09@mail.ru>
2014-12-20 23:40:42 +0300

    ДЗ4

--- red_new.php
+++ red_new.php
@@ -1,8 +1,8 @@
 <?php
-require 'boot.php';
-/*require_once __DIR__ . '/models/news.php';
-require_once __DIR__ . '/class/View.php';*/
+/*require 'boot.php';
+require_once __DIR__ . '/models/news.php';
+require_once __DIR__ . '/class/View.php';
 $controller = new NewsController();
 $controller->action('formared');
 //$redact = $st->Oun_new($id);
-//include('view/forma_redact.php');
+//include('view/forma_redact.php');*/
