40e035fa7129a829b05c35645b4484c0120389c9
ludmilas <alexss_09@mail.ru>
2014-12-20 23:39:58 +0300

    ДЗ4

--- new.php
+++ new.php
@@ -2,7 +2,7 @@
 
 /*require_once __DIR__ . '/models/news.php';
 require_once __DIR__ . '/class/View.php';*/
-/**$id=$_GET['id'];*/
+/**$id=$_GET['id'];
 require 'boot.php';
 $controller = new NewsController();
-$controller->action('ounnew');
+$controller->action('ounnew');*/
