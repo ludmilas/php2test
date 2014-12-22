92370cc411ec37abdf167c952ba515ccac8ba217
ludmilas <alexss_09@mail.ru>
2014-12-22 23:44:14 +0300

    ДЗ4

--- class/Newmod.php
+++ class/Newmod.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__ . '/Article.php';
+//require_once __DIR__ . '/Article.php';
 
-class Newmod extends Article {
+class Newmod extends Model {
 
-
+static protected $table = 'news';
 }
