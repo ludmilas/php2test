fb3924f26fa282b14fda07d528785ed8907a5bdd
ludmilas <alexss_09@mail.ru>
2014-12-22 22:09:07 +0300

    ДЗ4

---
+++ class/DbConnection.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class DbConnection extends Model {
+
+    static function config()
+    {
+        return include __DIR__ . '/../config.php';
+    }
+    static function getConnection()
+    {
+        $config = static::config();
+
+        $dsn = 'mysql:dbname=$config[db][dbname];host=$config[db][host]';
+        $dbh = new Pdo($dsn, $config[db][user], $config[db][password]);
+
+    }
+   static function findAll()
+   {
+       $sql = 'SELECT * FROM' . static::$table;
+       $dbh = static::getConnection();
+       $sth = $dbh->prepare($sql);
+       $sth->execute();
+       return $sth->fetchAll();
+
+
+   }
+
+
+
+
+}
