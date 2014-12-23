387a3211cbedbb293fdaef04580c58e8421bf60e
ludmilas <alexss_09@mail.ru>
2014-12-23 22:51:33 +0400

    ДЗ5

--- view/index.php
+++ view/index.php
@@ -7,8 +7,8 @@
 
      <?php foreach ($news as $article): ?>
     <article>
-        <h1><?=$article['title'];?></h1>
-        <div><?=$article['text'];?></div>
+        <h1><?=$article->title;?></h1>
+        <div><?=$article->text;?></div>
     </article>
     <?php endforeach; ?>
 
