526ae33429d00184cacad848292387bfdaf1a005
ludmilas <alexss_09@mail.ru>
2014-12-23 21:09:18 +0400

    ДЗ5

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 require 'boot.php';
 /*$controller = new NewsController();
 $controller->action('all');*/
-$route = $_GET['r'];
-$routeParts= explode('/', $route);
-$controllerClassName = ucfirst($routeParts[0]).'Controller';
+
+if (isset($_GET['r'])) {
+    $route = $_GET['r'];
+    $routeParts = explode('/', $route);
+    $controllerClassName = ucfirst($routeParts[0]) . 'Controller';
 
 
-$controller = new $controllerClassName;
-$controller->action($routeParts[1]);
+    $controller = new $controllerClassName;
+    $controller->action($routeParts[1]);
+}
+else
+{
+    $controller = new NewsController();
+    $controller->action('all');
+
+}
