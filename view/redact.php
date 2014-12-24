4fbd5a582021347ed35de41134ebf5c4a89e77d1
ludmilas <alexss_09@mail.ru>
2014-12-25 01:32:51 +0400

    ДЗ6

--- view/redact.php
+++ view/redact.php
@@ -1,6 +1,6 @@
 <h1>Ссылки на форму редактирования новостей</h1>
 <?php foreach ($news as $article): ?>
     <article>
-        <a href="index.php?id=<?=$article['id'];?>&r=news/formared"> Редактировать <?=$article['title'];?></a>
+        <a href="index.php?id=<?=$article->id;?>&r=news/formared"> Редактировать <?=$article->title;?></a>
     </article>
 <?php endforeach; ?>
